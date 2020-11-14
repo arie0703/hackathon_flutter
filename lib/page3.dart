0e2525c24470d4150956f484f753bb8d8c2bf149
Junaaaaa <sprintj24275hmit@gmail.com>
2020-11-15 02:49:19 +0900

    URL Launcher

--- lib/page3.dart
+++ lib/page3.dart
@@ -1,16 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class Page3 extends StatelessWidget {
+void main() => runApp(
+  MaterialApp(
+    home: Page3(),
+  ),
+);
+
+class Page3 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+}
+
+class _State extends State<Page3> {
+  var _status = 'Ready';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Text('page3'),
+            RaisedButton(
+              child: Text('Website'),
+              onPressed: () {
+                var url = 'https://flutter.keicode.com/';
+                _launchUrl('$url');
+              },
+            ),
+            RaisedButton(
+              child: Text('Email'),
+              onPressed: () {
+                var email = 'test@example.com';
+                var subject = Uri.encodeComponent('Hello');
+                var body = Uri.encodeComponent('This is a test.');
+                var url = 'mailto:$email?subject=$subject&body=$body';
+                print(url);
+                _launchUrl(url);
+              },
+            ),
+            RaisedButton(
+              child: Text('Phone'),
+              onPressed: () {
+                var tel = '+123456789';
+                _launchUrl('tel:$tel');
+              },
+            ),
+            RaisedButton(
+                child: Text('SMS'),
+                onPressed: () {
+                  var tel = '2345678901';
+                  _launchUrl('sms:$tel');
+                }),
+            Text(_status)
           ],
         ),
       ),
     );
+  }
+
+  _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      setState(() {
+        _status = 'Unable to launch url $url';
+      });
+    }
   }
 }
