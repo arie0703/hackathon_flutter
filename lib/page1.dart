709a60b53509d51d9a26a379e6969c0453dfa856
nikuitigogomi6 <yunayunayuuna.yuuna@gmail.com>
2020-11-15 14:00:33 +0900

    chat bot

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Page1 extends StatelessWidget {
   @override
@@ -27,7 +28,7 @@ class DialogueScreen extends StatefulWidget {
 class _DialogueScreen extends State<DialogueScreen> {
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = TextEditingController();
-
+  int n = 0;
   // Controllerはウィジェットの値を管理するクラス
   // TextFieldのような入力を行うウィジェットは自身の中に値を保管するプロパティを持っている訳でなく、
   // 値を管理するControllerクラスを組み込み、これによって値を管理する
@@ -64,21 +65,6 @@ class _DialogueScreen extends State<DialogueScreen> {
             )));
   }
 
-  void Response(text) async {
-    _textController.clear();
-    ChatMessage message = ChatMessage(
-      text: 'そっか〜',
-      name: "Bot",
-      type: false,
-    );
-    // setStateメソッドはステートの更新を、ステートクラスに知らせる働きをする
-    // このメソッドに、必要な値を変更する処理を実装する
-    // ここでは_messagesプロパティの値を変更し、messageを追加する
-    setState(() {
-      _messages.insert(0, message);
-    });
-  }
-
   void _handleSubmitted(String text) {
     _textController.clear();
     ChatMessage message = ChatMessage(
@@ -90,7 +76,88 @@ class _DialogueScreen extends State<DialogueScreen> {
     setState(() {
       _messages.insert(0, message);
     });
-    Response(text);
+    if (n == 0) {
+      initialMessage();
+      n += 1;
+    } else if(text == 'しんどい'){
+      ResponseCry();
+    }else{
+      Response()
+    }
+  }
+
+  void Response() async {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: 'そっか〜',
+      name: "すはらゆか",
+      type: false,
+    );
+    // setStateメソッドはステートの更新を、ステートクラスに知らせる働きをする
+    // このメソッドに、必要な値を変更する処理を実装する
+    // ここでは_messagesプロパティの値を変更し、messageを追加する
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void ResponseCry() async {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: '元気出してね〜',
+      name: "すはらゆか",
+      type: false,
+    );
+    // setStateメソッドはステートの更新を、ステートクラスに知らせる働きをする
+    // このメソッドに、必要な値を変更する処理を実装する
+    // ここでは_messagesプロパティの値を変更し、messageを追加する
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void initialMessage() {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: 'こんにちは。私は進路サポートボットすはらさん！',
+      name: "すはらゆか",
+      type: false,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    ChatMessage message1 = ChatMessage(
+      text: 'あなたが抱えている進路の不安解消をサポートできるように頑張るよ！',
+      name: "すはらゆか",
+      type: false,
+    );
+    setState(() {
+      _messages.insert(0, message1);
+    });
+    ChatMessage message2 = ChatMessage(
+      text: '誰でも不安は抱えるもの。私も大学受験の時「何がしたいんだろう…」って苦労した…',
+      name: "すはらゆか",
+      type: false,
+    );
+    setState(() {
+      _messages.insert(0, message2);
+    });
+    ChatMessage message3 = ChatMessage(
+      text: '一緒に好きなこと、興味のあることを見つけて、自信をつけるお手伝いができたら嬉しいな。',
+      name: "すはらゆか",
+      type: false,
+    );
+    setState(() {
+      _messages.insert(0, message3);
+    });
+    ChatMessage message4 = ChatMessage(
+      text: 'よろしくね！',
+      name: "すはらゆか",
+      type: false,
+    );
+    setState(() {
+      _messages.insert(0, message4);
+    });
   }
 
   // buildで生成されるウィジェットでは、ListViewのitemBuilder(表示部)とitemCount(要素数)に_messagesが使われている
@@ -121,6 +188,7 @@ class _DialogueScreen extends State<DialogueScreen> {
 }
 
 // dialogue_screen.dart
+
 class ChatMessage extends StatelessWidget {
   ChatMessage({this.text, this.name, this.type});
 
@@ -132,20 +200,20 @@ class ChatMessage extends StatelessWidget {
     return <Widget>[
       Container(
         margin: EdgeInsets.only(right: 16.0),
-        child: CircleAvatar(child: Image.asset("img/bot.png")),
+        child: CircleAvatar(child: Image.asset("images/suharacircle.png")),
       ),
       Expanded(
           child: Column(
-        // Columnに組み込んだウィジェットの配置場所を指定
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(this.name, style: TextStyle(fontWeight: FontWeight.bold)),
-          Container(
-            margin: EdgeInsets.only(top: 5.0),
-            child: Text(text),
-          ),
-        ],
-      ))
+            // Columnに組み込んだウィジェットの配置場所を指定
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(this.name, style: TextStyle(fontWeight: FontWeight.bold)),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ))
     ];
   }
 
@@ -182,3 +250,5 @@ class ChatMessage extends StatelessWidget {
     );
   }
 }
+
+
